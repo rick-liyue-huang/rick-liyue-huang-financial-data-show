f6e1dc756ccd825ce19b2cf7a4f5c327f61d7e9e
Rick Huang <rick.liyue.huang@gmail.com>
2024-02-18 23:38:09 +1100

    ci: add axios and get the real data

---
+++ client/src/api.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { CompanySearch } from './company';
+
+interface Response {
+  data: CompanySearch[];
+}
+
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<Response>(
+      `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('Axios Error:', error.message);
+      return error.message;
+    } else {
+      console.log('Unexpected Error:', error);
+      return 'An unexpected error occurred';
+    }
+  }
+};
